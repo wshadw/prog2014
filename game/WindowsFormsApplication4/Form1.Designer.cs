d88305957bf233577fb5f57eb89c2a556b48701e
wshadw <19wt@bk.ru>
2014-03-19 01:17:39 +0600

    +game

---
+++ game/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(145, 383);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ваш счёт: 0";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 500;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 10;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 10;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(180, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(29, 35);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Жизни: 3";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(107, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 42);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Коэффициент сложности: ";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(265, 11);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(21, 24);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(107, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 37);
+            this.label5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(176, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 20);
+            this.label6.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(500, 435);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Name = "Form1";
+            this.Text = "Игра";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ game/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public int CountTimer = 0;
+        public int sb = 2;
+        public int sr = 0;
+        public int sl = 0;
+        public float score = 0;
+        public int count = 0;
+        public int lives = 3;
+        public int temp1 = 0;
+        public int temp2 = 0;
+        public int spd = 1;
+        public int difficult = 1;
+        float kf = 0;        
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            Random rnd = new Random(DateTime.Now.Second * DateTime.Now.Millisecond);
+            pictureBox2.Top = -50;
+            pictureBox2.Left = rnd.Next(0, this.Width-50);
+            pictureBox3.Top = -50;
+            pictureBox3.Left = rnd.Next(0, this.Width - 50);
+            timer3.Start();
+
+          
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            label4.Text = String.Format("({0}, {1}) {2}x{3}",
+            this.Location.X, this.Location.Y, this.Width, Height);
+        } 
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyData == Keys.Right) timer1.Start(); else timer1.Stop();
+            if (e.KeyData == Keys.Left) timer2.Start(); else timer2.Stop();
+        }
+        private void Form1_KeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+         timer5.Start();
+         sl = sr = 0;
+         timer1.Stop();
+         timer2.Stop();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sl++;
+            if (sl < 3) pictureBox1.Left += 2;  else pictureBox1.Left += 2 + spd;
+            if (pictureBox1.Left > this.Width - 75) pictureBox1.Left = -50;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            sr++;
+            if (sr < 3) pictureBox1.Left -= 2; else pictureBox1.Left -= 2 + spd;
+            if (pictureBox1.Left < -50) pictureBox1.Left = this.Width - 75;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            kf = ((float)this.Width / Height) * ((float)this.Width / Height);
+            label4.Text = "Коэффициент сложности: " + Math.Round(kf, 1);
+            label1.Text = "Ваш счёт: " + Math.Round(score, 0);
+            label2.Text = "Жизни: " + lives;
+            label6.Text = "";
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random(DateTime.Now.Second * DateTime.Now.Millisecond);
+            pictureBox3.Left += sb;
+            pictureBox3.Top += Math.Abs(sb/2);
+            if (pictureBox3.Left > this.Width - 30||pictureBox3.Left< 0) sb = -sb;
+            if (pictureBox3.Left > pictureBox1.Left + 8 && pictureBox3.Left < pictureBox1.Left + 68 &&
+                pictureBox3.Top > Height - 100 && pictureBox3.Top < Height - 60)
+            {
+                if (spd < 5) spd++;
+                lives++;
+                pictureBox3.Top = -50;
+                pictureBox3.Left = random.Next(0, this.Width - 50);
+                if (spd < 5) label3.Text = "Скорость - х" + spd; else label3.Text = "Скорость MAX";
+                label5.Text = "Жизни - " + lives;
+                CountTimer = 50;
+                timer4.Stop();
+            }
+            if (pictureBox3.Top > Height - 30) { 
+            pictureBox3.Top = -50;
+            pictureBox3.Left = random.Next(0, this.Width - 50);
+            timer4.Stop();
+            }
+        }
+
+
+        public class result
+        {
+            
+        }
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            label6.Left = pictureBox1.Left+30;
+            label6.Top = pictureBox1.Top - 50;
+            if (CountTimer > 0) CountTimer--;
+            if (CountTimer == 1) { label3.Text = ""; label5.Text = ""; } 
+            Random random = new Random(DateTime.Now.Second*DateTime.Now.Millisecond);
+            
+            int popadanie=0;
+
+            pictureBox2.Top += 2 + difficult;
+            if (pictureBox2.Left > pictureBox1.Left + 12 && pictureBox2.Left < pictureBox1.Left + 64 &&
+                pictureBox2.Top > Height - 100 && pictureBox2.Top < Height - 60)
+            {
+                label6.Text = "+" + Math.Round(100 * kf * difficult, 0);
+                lives++;
+                count++;
+                score = (score/kf + 100 * difficult)*kf;
+                popadanie = 1;
+
+            }
+
+            if (pictureBox2.Top > Height - 30 || popadanie == 1)
+            {
+                pictureBox2.Top = -50;
+                pictureBox2.Left = random.Next(0, this.Width - 50);
+                lives--;
+            }
+   //         label1.Text = "Ваш счёт: " + Math.Round(score, 0);
+   //         label2.Text = "Жизни: " + lives;
+            temp1 = count/10;
+            if (temp1 > temp2)
+            {
+                temp2 = temp1;
+                difficult++;
+                label3.Text = "Уровень " + difficult;
+                CountTimer = 35;
+            //    if (spd<5)spd++;
+                timer4.Start();
+            }
+            if (lives==0)
+            {
+                label3.Text="Вы проиграли!"; 
+                timer5.Stop();
+                var res = MessageBox.Show("Начать сналчала?", "Вы проиграли!", MessageBoxButtons.YesNo);
+                if (res == DialogResult.Yes)
+                {
+                    score = 0;
+                    difficult=1;
+                    lives = 3;
+                    temp1 = 0;
+                    temp2 = 0;
+                    spd = 1;
+                    count = 0;
+                    label3.Text="";
+                    timer5.Start();
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lives = 3;
+            count = 0;
+            timer5.Start();
+        }
+    }
+}
